a5c45d7375c28857d82a83710defe5108bb3f893
Wryhder <jemimahsummers@yahoo.com>
2018-10-16 16:15:44 +0100

    Create Hero class object

--- js/app.js
+++ js/app.js
@@ -1,7 +1,70 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Hero {
+    constructor() {
+        this.step = 101;
+        this.jump = 83;
+        this.startX = this.step * 2;
+        this.startY = (this.jump * 4) + 60;
+        this.x = this.startX;
+        this.y = this.startY;
+        this.wonGame = false;
+        this.sprite = 'images/char-princess-girl.png';
+    }
+
+    // Draw hero sprite on current x and y coord position
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    update() {
+        // Update position
+        for (let enemy of allEnemies) {
+            // Check collision here
+            // Did player x and y collide with enemy's?
+            if (this.y === enemy.y &&
+                (enemy.x + enemy.step / 2 > this.x &&
+                    enemy.x < this.x + this.step / 2)) {
+                this.resetHeroPos();
+            }
+        }
+
+        // Check win here
+        // Did player's x and y reach final tile?
+        if (this.y === 60) {
+            this.wonGame = true;
+        }
+    }
+
+    handleInput(input) {
+        // Update player sprite on current x and y coord position
+        switch (input) {
+            case 'up':
+                if (this.y > this.jump) {
+                    this.y -= this.jump;
+                }
+                break;
+            case 'down':
+                if (this.y < this.jump * 4) {
+                    this.y += this.jump;
+                }
+                break;
+            case 'left':
+                if (this.x > 0) {
+                    this.x -= this.step;
+                }
+                break;
+            case 'right':
+                if (this.x < this.step * 4) {
+                    this.x += this.step;
+                }
+        }
+    }
+
+    resetHeroPos() {
+        // Set x and y to starting x and y
+        this.x = this.startX;
+        this.y = this.startY;
+    }
+}
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
