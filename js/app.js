a11ecc6297042213da928ba961b058d83f5cf475
Wryhder <jemimahsummers@yahoo.com>
2018-10-16 16:17:53 +0100

    Convert var to const

--- js/app.js
+++ js/app.js
@@ -111,8 +111,8 @@ allEnemies.push(enemy1, enemy2, enemy3);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+document.addEventListener('keyup', function (e) {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
